f7c55f13140846e245efbad3178fb4929e4639ac
Nikhilkrishnas <42650316+Nikhilkrishnas@users.noreply.github.com>
2018-09-26 22:54:18 -0500

    Create a registration page.

---
+++ ASEProject/app/src/main/java/com/example/thota/aseproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.thota.aseproject;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+public final int TIME_OUT= 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setBackgroundColor(Color.CYAN);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent pass= new Intent(MainActivity.this, Login.class);
+                startActivity(pass);
+                finish();
+            }
+        },TIME_OUT);
+    }
+}
